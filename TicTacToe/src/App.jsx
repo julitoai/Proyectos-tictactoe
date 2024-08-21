10f83873faac0a582863918d4605344042c7d8f6
Julian <juliamruani@gmail.com>
2024-08-21 16:07:55 -0300

    ticTacToe

---
+++ TicTacToe/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react"
+
+const TURNS = {
+  X: "x",
+  O: "o"
+}
+
+const Square = ({children, updateBoard, index }) => {
+  return(
+    <div className="square">
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
+  
+  return (
+
+    <main className="board">
+
+     <h1>Tic Tac Toe</h1>
+     <section className="game">
+      {
+      board.map((_, index) => {
+        return (
+          <Square
+          key={index}
+          index={index}
+          >
+          {index}
+          </Square>  
+        )
+      }) 
+       
+      }
+
+     </section>
+
+     <section className="turn">
+      <Square isSelected ={}>{TURNS.X}</Square>
+      <Square>{TURNS.O}</Square>
+     </section>
+       
+
+    </main>
+  )
+}
+
+export default App
