ebec2fe0dc422a8a0002e8789403b9b3f5d4c0ca
Julian <juliamruani@gmail.com>
2024-09-06 18:29:11 -0300

    login-validacion-01

--- LogIn/src/Login.jsx
+++ LogIn/src/Login.jsx
@@ -1,14 +1,33 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
 
+  const handleInputSubmit = (e) =>{
+    e.preventDefault();
+    console.log("onchange funcionando")
+      const { name, value } = e.target;
+      setFormValues({
+        ...formValues,
+        [name]:value
+      })
+  }
 
+  const [formValues, setFormValues] = useState({
+    input1:'',
+    input2:''
+});
+  const [error, setError] = useState(" ");
 
-  const isCompleted = () =>{
-      if(input = null){
-        return console.log('isCompleted valido')
-      }
-  } 
+  const validacionInputs = () => {
+    if(formValues.input1 === ''){
+      setError('Este campo es obligatorio')
+    }else if(formValues.input2 === ''){
+      setError('Este campo es obligatorio')
+    }else{
+      setError('')
+    }
+  }
 
   return (
     <>
@@ -16,28 +35,45 @@ const Login = () => {
       <div className="container-section">
         <section className="section-box">
           <h2>Register</h2>
-          <form className="form">
+          <form onSubmit={handleInputSubmit} className="form">
             <span className="span-form">
             <h3 className="register-text">Name</h3>
             <input
-              required
+              value={formValues.input1}
+              name="input1"
               className="input-form"
               type="text"
               placeholder="Enter your name"
+              onChange={(e) => {
+                setFormValues({
+                  ...formValues,
+                  input1: e.target.value
+                })
+                validacionInputs();
+              }}
             />
             </span>
             <span className="span-form">
             <h3 className="register-text">Email</h3>
             <input
-              required
+              value={formValues.input2}
+              name="input2"
               className="input-form"
               type="text"
               placeholder="Enter your E-mail"
+              onChange={(e) => {
+                setFormValues({
+                  ...formValues,
+                  input2: e.target.value
+                })
+                validacionInputs();
+              }}
             />
             </span>
+            {error && <p>{error}</p>}
             <span className="span-button">
-            <button onClick={isCompleted} className="button-form"><Link className="link" to={`/Topics`}>Continue</Link></button>
-            </span>
+            <button type="submit" className="button-form"><Link className="link" to={`/Topics`}>Continue</Link></button>
+            </span>            
           </form>
         </section>
       </div>
