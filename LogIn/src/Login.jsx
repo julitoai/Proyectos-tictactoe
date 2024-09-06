484cf60c48858a613183cbaf95b745561d4bc21e
Julian <juliamruani@gmail.com>
2024-09-06 02:09:16 -0300

    commit

--- LogIn/src/Login.jsx
+++ LogIn/src/Login.jsx
@@ -2,8 +2,12 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
 
+
+
   const isCompleted = () =>{
-    
+      if(input = null){
+        return console.log('isCompleted valido')
+      }
   } 
 
   return (
@@ -12,10 +16,11 @@ const Login = () => {
       <div className="container-section">
         <section className="section-box">
           <h2>Register</h2>
-          <form className="form" action="get">
+          <form className="form">
             <span className="span-form">
             <h3 className="register-text">Name</h3>
             <input
+              required
               className="input-form"
               type="text"
               placeholder="Enter your name"
@@ -24,14 +29,14 @@ const Login = () => {
             <span className="span-form">
             <h3 className="register-text">Email</h3>
             <input
+              required
               className="input-form"
               type="text"
               placeholder="Enter your E-mail"
             />
             </span>
             <span className="span-button">
-            <button className="button-form"><Link className="link" to={`/Topics`}>Continue</Link></button>
-
+            <button onClick={isCompleted} className="button-form"><Link className="link" to={`/Topics`}>Continue</Link></button>
             </span>
           </form>
         </section>
